ffba1a6abfc49a70f646cd5fc38efbf20233da18
phongnn <phongnn@gmail.com>
2020-09-24 17:45:17 +0800

    Update config files

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     "\\.tsx?$": "ts-jest",
   },
+  roots: ["<rootDir>/src"],
   testPathIgnorePatterns: ["__setup__.ts"],
   setupFilesAfterEnv: ["<rootDir>/src/__tests__/__setup__.ts"],
   clearMocks: true,
